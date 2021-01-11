17e296586f9c2e02e008726a6e567635bb17ce57
bTsuyoi <gubbala.saikishore@gmail.com>
2021-01-11 11:08:22 +0530

    ADDED:  code to repeat the die roll till the player reaches the winning position 100 and to ensure the player position never moves below 0.

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -7,24 +7,32 @@ public class snakeAndLadder {
 		int position = 0;
 
 		System.out.println("The position of the player was: "+ position);
-		double diceVal = Math.floor(Math.random() * 10) % 6 + 1;
-		double option = Math.floor(Math.random() * 10) % 3;
+		while (position <=100) {
+			double diceVal = Math.floor(Math.random() * 10) % 6 + 1;
+			double option = Math.floor(Math.random() * 10) % 3;
 
-		switch ((int)option) {
-		case IS_SNAKE:
-			position -= diceVal;
-			System.out.println("Number on the dice: "+ diceVal);
-			System.out.println("Player got snake so moved back to position: " + position);
-			break;
-		case IS_LADDER:
-			position += diceVal;
-			System.out.println("Number on the dice: "+ diceVal);
-			System.out.println("Player got ladder so moved ahead to position: " + position);
-			break;
-		default:
-			System.out.println("Player didn't play so current position: " + position);
+			switch ((int)option) {
+			case IS_SNAKE:
+				position -= diceVal;
+				if (position < 0) {
+					position = 0;
+					System.out.println("Number on the dice: "+ diceVal);
+					System.out.println("Since player can't go behind position 0 player stays at position: " + position);
+				}
+				else
+					System.out.println("Number on the dice: "+ diceVal);
+					System.out.println("Player got snake so moved back to position: " + position);
+				break;
+			case IS_LADDER:
+				position += diceVal;
+				System.out.println("Number on the dice: "+ diceVal);
+				System.out.println("Player got ladder so moved ahead to position: " + position);
+				break;
+			default:
+				System.out.println("Player didn't play so current position: " + position);
+			}
+
 		}
-
-
 	}
+
 }
